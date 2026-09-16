ef5a4c2edc6d736dd1138c06cf2e213e84b0ebe7
Raul Popa Monforte <raulpopam@gmail.com>
2026-09-16 16:33:55 +0200

    funcionalidad de saludo dependiente de la hora del día

--- src/main/kotlin/controller/HelloController.kt
+++ src/main/kotlin/controller/HelloController.kt
@@ -8,6 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+//Imports usados para el cálculo identificar si es por la mañana o
+//por la tarde
+import java.time.Instant
+import java.time.ZoneId
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.ZonedDateTime
+
 @Controller
 class HelloController(
     @param:Value("\${app.message:Hello World}") 
@@ -35,5 +43,40 @@ class HelloApiController {
             "message" to "Hello, $name!",
             "timestamp" to java.time.Instant.now().toString()
         )
+    }
+}
+
+@RestController
+class TimeDependingController {
+
+    
+    fun timeDependingGreeting(): Pair<String, ZonedDateTime> 
+    {
+        var greeting : String
+        val tiempoActual : Instant = java.time.Instant.now()
+        val IdZona = ZoneId.of("Europe/Madrid")
+        val medioDia = LocalDate.now(IdZona).atTime(LocalTime.NOON).atZone(IdZona)
+                                .toInstant()
+
+        val tiempoDevolver = tiempoActual.atZone(IdZona)
+        if (tiempoActual.isAfter(medioDia)) {
+            return Pair("Buenas tardes", tiempoDevolver)
+        } else {
+            return Pair("Buenos días", tiempoDevolver)
+        }
+    }
+
+    @GetMapping("/api/helloTime", produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun greetingTime(@RequestParam(defaultValue = "Desconocido") nombre : String)
+        : Map<String,String>  
+    {
+            val resultado = timeDependingGreeting()
+            val saludo = resultado.first
+            val tiempo = resultado.second.toString()
+
+            return mapOf(
+                "message" to "$saludo, $nombre",
+                "timestamp" to "$tiempo"
+            )    
     }
 }
